b48ec70312634faf47ae09e603ed30c102769254
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-27 15:33:22 +0100

    Added readme, and client side parsing of query results.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-iwa-tutorial
-============
+#Tutorial for the Intelligent Web Applications course 
+### @VU University Amsterdam
+#### Rinke Hoekstra (<rinke.hoekstra@vu.nl>)
 
-Tutorial for the Intelligent Web Applications course @VU University Amsterdam
+This tutorial shows the basics for building an Intelligent Web Application by guiding you through some of the typical steps you need to go through. This involves:
+
+* Styling your webpage using CSS
+* Using client side scripting for manipulating the webpage
+* Using server side scripting for mediating between your own triple store, and others
+
+### Requirements
+
+To get this thing up and running on a local machine, you need the following:
+
+* Python 2.7
+* The `flask`, `python-requests` and `sparqlwrapper` packages (install using `pip install <package name>`)
+* A recent Apache Tomcat install with deployed Sesame/OWLIM Lite
+* An OWLIM-Lite repository with (at a minimum) the OWL-Horst ruleset, and the name `tutorial`. If you want to use a different name or location (i.e. not running on localhost, port 8080) you need to set the `TUTORIAL_REPOSITORY` variable in `src/tutorial.py`
+
--- src/static/js/tutorial.js
+++ src/static/js/tutorial.js
@@ -433,10 +433,69 @@ $('#link13').on('click', function(e){
 	var format = 'JSON';
 	
 	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
-		console.log(json);
 		var pre = $('<pre></pre>');
 		pre.text(JSON.stringify(json));
 		$('#linktarget13').html(pre);
+	});
+	
+});
+
+
+/*
+// ############
+//    STEP 14
+// ############
+*/
+
+$('#link14').on('click', function(e){
+	
+	var query = $('#query14').val();
+	var endpoint = 'http://localhost:8080/openrdf-sesame/repositories/tutorial';
+	var format = 'JSON';
+	
+	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		console.log(json);
+		
+		try {
+			var vars = json.head.vars;
+		
+			var ul = $('<ul></ul>');
+			ul.addClass('list-group');
+		
+			$.each(json.results.bindings, function(index,value){
+				var li = $('<li></li>');
+				li.addClass('list-group-item');
+			
+				$.each(vars, function(index, v){
+					var v_type = value[v]['type'];
+					var v_value = value[v]['value'];
+				
+					li.append('<strong>'+v+'</strong><br/>');
+				
+					// If the value is a URI, create a hyperlink
+					if (v_type == 'uri') {
+						var a = $('<a></a>');
+						a.attr('href',v_value);
+						a.text(v_value);
+						li.append(a);
+					// Else we're just showing the value.
+					} else {
+						li.append(v_value);
+					}
+					li.append('<br/>');
+					
+				});
+				ul.append(li);
+			
+			});
+			
+			$('#linktarget14').html(ul);
+		} catch(err) {
+			$('#linktarget14').html('Something went wrong!');
+		}
+		
+
+		
 	});
 	
 });
