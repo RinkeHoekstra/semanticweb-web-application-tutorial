2989475c5446b85ce2690c7ed962d29a2a89a39d
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-27 11:43:21 +0100

    Added until step 9, where we actually fire a SPARQL query based on a service response.

--- src/static/js/tutorial.js
+++ src/static/js/tutorial.js
@@ -1,22 +1,36 @@
+
+/*
+// ############
+//    STEP 1
+// ############
+*/
+
 // If the element with id 'link1' is clicked
 $('#link1').on('click', function(e){
 	var bootstrapCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">');
 	var bootstrapThemeCSSLink = $('<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">');
 	var bootstrapJavaScriptLink = $('<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>');
 	
-	var codemirrorCSSLink = $('<link rel="stylesheet" type="text/css" href="/static/css/codemirror.css">');
-	var codemirrorJavaScriptLink = $('<script type="text/javascript" src="/static/js/codemirror-compressed.js"></script>');
-	
 	$('body').append(bootstrapCSSLink);
 	$('body').append(bootstrapThemeCSSLink);
-	$('body').append(bootstrapJavaScriptLink);
-	
-	$('body').append(codemirrorCSSLink);
-	$('body').append(codemirrorJavaScriptLink);
+	$('body').append(bootstrapJavaScriptLink);	
 });
 
+/*
+// ############
+//    STEP 2
+// ############
+*/
 
 
+// No JavaScript involved!
+
+
+/*
+// ############
+//    STEP 3
+// ############
+*/
 
 // If the element with id 'link3' is clicked
 $('#link3').on('click',function(e){
@@ -27,6 +41,11 @@ $('#link3').on('click',function(e){
 	});
 });
 
+/*
+// ############
+//    STEP 4
+// ############
+*/
 
 // If the element with id 'link4' is clicked
 $('#link4').on('click',function(e){
@@ -40,10 +59,188 @@ $('#link4').on('click',function(e){
 	});
 });
 
+/*
+// ############
+//    STEP 5
+// ############
+*/
+
 // If the element with id 'link5' is clicked
 $('#link5').on('click',function(e){
 	
 	var message = $('#messageInput2').val();
 	$('#linktarget5').html(message);
 	
+});
+
+
+/*
+// ############
+//    STEP 6
+// ############
+*/
+
+// Autocompletion from Linked Life Data
+
+// http://linkedlifedata.com/autocomplete.json?
+// Parameters are 'q' for query and some limit.
+// Note that we have to add a 'callback=?' suffix to the URI, to make sure the call is a JSONP request.
+$('#messageInput3').on('input', function(e){
+	var message = $('#messageInput3').val();
+	
+	var lld_autocomplete_url = 'http://linkedlifedata.com/autocomplete.json?callback=?';
+	
+	var data = {'q': message, 'limit': 100}
+	
+	$.getJSON(lld_autocomplete_url, data=data, function(json){
+		var pre = $('<pre></pre>');
+		pre.text(JSON.stringify(json));
+		$('#linktarget6').html(pre);
+	});
+});
+
+
+/*
+// ############
+//    STEP 7
+// ############
+*/
+
+// Autocompletion from Linked Life Data
+
+// http://linkedlifedata.com/autocomplete.json?
+// Parameters are 'q' for query and some limit.
+// Note that we have to add a 'callback=?' suffix to the URI, to make sure the call is a JSONP request.
+$('#messageInput7').on('input', function(e){
+	var message = $('#messageInput7').val();
+	
+	var lld_autocomplete_url = 'http://linkedlifedata.com/autocomplete.json?callback=?';
+	
+	var data = {'q': message, 'limit': 100}
+	
+	$.getJSON(lld_autocomplete_url, data=data, function(json){
+		
+		var ul = $('<ul></ul>');
+		ul.addClass('list-group');
+		for (var i in json.results) {
+			var r = json.results[i];
+			
+			var uri = r.uri.namespace + r.uri.localName;
+			var label = r.label;
+			
+			var li = $('<li></li>');
+			li.addClass('list-group-item');
+			var a = $('<a></a>');
+			a.html(label);
+			a.attr('href',uri);
+			li.append(a);
+			ul.append(li);
+		}
+		
+		$('#linktarget7').html(ul);
+	});
+});
+
+
+/*
+// ############
+//    STEP 8
+// ############
+*/
+
+
+// Autocompletion from Linked Life Data
+
+// http://linkedlifedata.com/autocomplete.json?
+// Parameters are 'q' for query and some limit.
+// Note that we have to add a 'callback=?' suffix to the URI, to make sure the call is a JSONP request.
+$('#messageInput8').on('input', function(e){
+	var message = $('#messageInput8').val();
+	
+	var lld_autocomplete_url = 'http://linkedlifedata.com/autocomplete.json?callback=?';
+	
+	var data = {'q': message, 'limit': 100}
+	
+	$.getJSON(lld_autocomplete_url, data=data, function(json){
+		
+		var ul = $('<ul></ul>');
+		ul.addClass('list-group');
+		for (var i in json.results) {
+			var r = json.results[i];
+			
+			var uri = r.uri.namespace + r.uri.localName;
+			var label = r.label;
+			
+			var li = $('<li></li>');
+			li.addClass('list-group-item');
+			var a = $('<a></a>');
+			a.html(label);
+			
+			a.on('click', function(e){
+				alert('You clicked '+ uri);
+			});
+			
+			li.append(a);
+			ul.append(li);
+		}
+		
+		$('#linktarget8').html(ul);
+	});
+});
+
+
+/*
+// ############
+//    STEP 9
+// ############
+*/
+
+
+// Autocompletion from Linked Life Data
+
+// http://linkedlifedata.com/autocomplete.json?
+// Parameters are 'q' for query and some limit.
+// Note that we have to add a 'callback=?' suffix to the URI, to make sure the call is a JSONP request.
+$('#messageInput9').on('input', function(e){
+	var message = $('#messageInput9').val();
+	
+	var lld_autocomplete_url = 'http://linkedlifedata.com/autocomplete.json?callback=?';
+	
+	var data = {'q': message, 'limit': 100}
+	
+	$.getJSON(lld_autocomplete_url, data=data, function(json){
+		
+		var ul = $('<ul></ul>');
+		ul.addClass('list-group');
+		for (var i in json.results) {
+			var r = json.results[i];
+			
+			var uri = r.uri.namespace + r.uri.localName;
+			var label = r.label;
+			
+			var li = $('<li></li>');
+			li.addClass('list-group-item');
+			var a = $('<a></a>');
+			a.html(label);
+			
+			a.on('click', function(e){
+				
+				var query = 'DESCRIBE <'+uri+'>'
+				var endpoint = 'http://linkedlifedata.com/sparql.rdf';
+				
+				$.get('/sparql/rdf',data={'endpoint': endpoint, 'query': query}, function(json){
+					console.log(json);
+					var pre = $('<pre></pre>');
+					pre.text(JSON.stringify(json));
+					$('#linktarget9').html(pre);
+				});
+				
+			});
+			
+			li.append(a);
+			ul.append(li);
+		}
+		
+		$('#linktarget9').html(ul);
+	});
 });
--- src/tutorial.py
+++ src/tutorial.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
+from SPARQLWrapper import SPARQLWrapper, RDF, JSON
+import json
+
 
 app = Flask(__name__)
 
@@ -16,6 +19,48 @@ def show_message():
     message = request.args.get('message', 'No message sent!')
     
     return render_template('message.html', message=message)
+    
+@app.route('/sparql', methods=['GET'])
+def sparql_rdf():
+    app.logger.debug('You arrived at ' + url_for('sparql_rdf'))
+    app.logger.debug('I received the following arguments' + str(request.args) )
+    
+    endpoint = request.args.get('endpoint', None)
+    query = request.args.get('query', None)
+    
+    return_format = request.args.get('format','JSON')
+    
+    
+    if endpoint and query :
+        sparql = SPARQLWrapper(endpoint)
+        
+        sparql.setQuery(query)
+        
+        if return_format == 'RDF':
+            sparql.setReturnFormat(RDF)
+        else :
+            sparql.setReturnFormat('JSON')
+        
+        
+        app.logger.debug('Querying endpoint {}'.format(endpoint))
+        
+        try :
+            response = sparql.query().convert()
+            
+            app.logger.debug('Results were returned, yay!')
+            
+            app.logger.debug(response)
+            
+            if return_format == 'RDF':
+                return response.serialize(format='turtle')
+            else :
+                return response
+        except :
+            return jsonify({'result': 'Error'})
+            
+        
+    else :
+        return jsonify({'result': 'Error'})
     
     
 if __name__ == '__main__':
